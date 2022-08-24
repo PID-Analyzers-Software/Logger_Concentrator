6debec0c651d5f38ca51bebb676f42c1d2a0a2f9
Zhijie Wang <zjwa127@gmail.com>
2022-08-24 10:31:43 +0800

    Update sketch_aug22a.ino

--- sketch_aug22a/sketch_aug22a.ino
+++ sketch_aug22a/sketch_aug22a.ino
@@ -10,7 +10,7 @@
 #endif
 
 BluetoothSerial SerialBT;
-
+int timelap = 0;
 
 // set pin numbers
 const int buttonPin = 22;  // the number of the pushbutton pin
@@ -23,10 +23,6 @@ int time_remain = millis();
 #define wifi_ssid             "22CD"
 #define wifi_password       "00525508"
 
-// See https://thingsboard.io/docs/getting-started-guides/helloworld/
-// to understand how to obtain an access token
-#define TOKEN               "tbX413nxQeUdP8AqW6cE"
-#define THINGSBOARD_SERVER  "thingsboard.cloud"
 
 // Baud rate for debug serial
 #define SERIAL_DEBUG_BAUD   115200
@@ -54,30 +50,23 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
-  
+  delay(100);
   int time_now =  millis();
 
-  int16_t adc0;
-  adc0 = ads.readADC_SingleEnded(0);
-  float Voltage = (adc0 * 0.1875) / 1000;
-
-  int avg = avgInput.reading(Voltage);
+  float Voltage = analogRead(4) * 3300 / 4095;
 
   buttonState = digitalRead(buttonPin);
   if (buttonState == HIGH) {
-    // turn LED on
     time_remain = time_now + 5 * 60 * 1000;
   }
 
-  Serial.print(time_now);
-  Serial.print("  ");
-  Serial.println(time_remain - time_now);
-
   if ((time_remain - time_now) > 0) {
-    SerialBT.write(avg);
-    Serial.println("Sending data...");
+    if (timelap < time_now) {
+      SerialBT.print(("" + String(Voltage, 0) + ",mV, \n").c_str());
+      Serial.println(Voltage);
+      Serial.println("Sending data...");
+      timelap = time_now + 1000;
+    }
   }
-
 
 }
